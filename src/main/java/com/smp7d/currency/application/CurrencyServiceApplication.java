4a3d3ab68197828e3b8675be121216b32ef71eb4
steveprim <smp7dg@gmail.com>
2018-01-15 13:06:14 -0500

    bootstrap this app

---
+++ src/main/java/com/smp7d/currency/application/CurrencyServiceApplication.java
@@ -0,0 +1,12 @@
+package com.smp7d.currency.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyServiceApplication.class, args);
+	}
+}
